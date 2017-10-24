468acca6a216bce42b31fe88cc56246dc5a9b779
Rafael Rollo <rafaelrollo92@gmail.com>
2017-10-24 16:07:16 -0200

    Corrigindo referencia ao host no ambiente do heroku

--- src/main/java/br/com/alura/instalura/Boot.java
+++ src/main/java/br/com/alura/instalura/Boot.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
-@Controller
 public class Boot {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/alura/instalura/security/LoginController.java
+++ src/main/java/br/com/alura/instalura/security/LoginController.java
@@ -2,6 +2,11 @@ package br.com.alura.instalura.security;
 
 import java.net.URI;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
@@ -15,13 +20,16 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @CrossOrigin
 public class LoginController {
+	
+	@Value("${server.port}")
+	private String SERVER_PORT;
 
 	@PostMapping(value = "/api/public/login", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> autentica(@RequestBody LoginDTO loginDTO) {
 		RestTemplate restTemplate = new RestTemplate();
 
 		RequestEntity<LoginDTO> request = RequestEntity
-				.post(URI.create("http://localhost:8080/api/login"))
+				.post(URI.create("http://localhost:" + SERVER_PORT + "/api/login"))
 				.contentType(MediaType.APPLICATION_JSON).body(loginDTO);
 		try {
 			ResponseEntity<String> response = restTemplate.exchange(request, String.class);
